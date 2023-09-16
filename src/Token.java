133df782d0b76befdb4fc9d9779299f6cfd55c35
slicar2640 <101480013+slicar2640@users.noreply.github.com>
2023-09-16 17:07:37 -0500

    Add variables

--- src/Token.java
+++ src/Token.java
@@ -1,8 +1,10 @@
 public class Token {
     String type;
     String lexeme;
-    public Token(String type, String lexeme) {
+    double value;
+    public Token(String type, String lexeme, double value) {
         this.type = type;
         this.lexeme = lexeme;
+        this.value = value;
     }
 }
